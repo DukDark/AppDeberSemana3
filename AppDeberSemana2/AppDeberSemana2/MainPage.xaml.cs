48bd76835ba13f66759ed60720d3a7083bbf0cc9
pato_ <pato_@DESKTOP-I56NTAN>
2021-10-27 16:53:01 -0500

    Agregar archivos de proyecto.

---
+++ AppDeberSemana2/AppDeberSemana2/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppDeberSemana2
+{
+    public partial class MainPage : ContentPage
+    {
+        string usuariolog;
+        public MainPage(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+            usuariolog = usuario;
+
+        }
+
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double valor1 = Convert.ToDouble(txtValor1.Text);
+                double valor2 = Convert.ToDouble(txtValor2.Text);
+                double valor3 = Convert.ToDouble(txtValor3.Text);
+                double valor4 = Convert.ToDouble(txtValor4.Text);
+
+                
+
+                double porcen1 = valor1 * 0.3;
+                double porcen2 = valor2 * 0.2;
+                double suma = porcen1 + porcen2;
+                double porcen3 = valor3 * 0.3;
+                double porcen4 = valor4 * 0.2;
+                double suma1 = porcen3 + porcen4;
+                double suma2 = suma + suma1;
+                if (suma2>=5)
+                {
+
+                txtPorcen1.Text = porcen1.ToString();
+                txtPorcen2.Text = porcen2.ToString();
+                txtResultado.Text = suma.ToString();
+                txtPorcen3.Text = porcen3.ToString();
+                txtPorcen4.Text = porcen4.ToString();
+                txtResultado1.Text = suma1.ToString();
+                txtValor5.Text = suma2.ToString();
+                    DisplayAlert("Aprobado", "Felicidades por aprobar el curso", "ok");
+                  }
+                else
+                {
+                    DisplayAlert("Reprobado", "Su nota es menor a 5 por lo tanto, no podra rendir examen de recuperacion", "ok");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de alerta", ex.Message, "Ok");
+            }
+        }
+
+        
+
+        private void btnSumar2_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ AppDeberSemana2/AppDeberSemana2/viewDos.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppDeberSemana2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class viewDos : ContentPage
+    {
+        public viewDos()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string usuario = txtUsuario.Text;
+                string clave = txtClave.Text;
+
+                if (usuario == "estudiante2021" && clave == "uisrael2021")
+                {
+                    await Navigation.PushAsync(new MainPage(usuario));
+                }
+                else
+                {
+                    await DisplayAlert("Datos Incorrectos", "Usuario y/o contraseña incorrecta", "ok");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Mensaje de alerta", ex.Message, "ok");
+            }
+        }
+    }
+
+    
+}
